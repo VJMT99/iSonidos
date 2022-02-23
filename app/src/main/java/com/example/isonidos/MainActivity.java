51d753ecf2accff71e0589dfb33907be0dc7729a
Victor Jimenez <vjmt99@gmail.com>
2022-02-23 17:38:47 +0100

    commit2

--- app/src/main/java/com/example/isonidos/MainActivity.java
+++ app/src/main/java/com/example/isonidos/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.isonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         int idSonido = R.raw.tengo_miedo;
         m = MediaPlayer.create(this, idSonido);
         m.start();
+    }
+    public void reproduceVideo(View vista){
+        VideoView v = findViewById(R.id.videoview);
+        int idVideo = this.getResources().getIdentifier(vista.getTag().toString(), "raw", this.getPackageName());
+        Uri uri =Uri.parse("android.resource://" + getPackageName() + "/" + idVideo);
+        v.setVideoURI(uri);
+        v.start();
     }
 }
